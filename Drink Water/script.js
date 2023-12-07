27723a38d914aa043e4ce3583b4342216f40bee7
Francesco <francescod1188@gmail.com>
2023-12-07 15:29:55 -0500

    Initial Commit

---
+++ Drink Water/script.js
@@ -0,0 +1,53 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, x) => {
+    cup.addEventListener('click',() => highlightCups(x))
+})
+
+function highlightCups(x){
+
+    if(smallCups[x].classList.contains('full') && !smallCups[x].nextElementSibling.classList.contains('full')){
+        x--
+    }
+    smallCups.forEach((cup, x2) => {
+        if(x2 <= x){
+            cup.classList.add('full')
+        }
+        else{
+            cup.classList.remove('full')
+        }
+    })
+
+    updateBigCup()
+}
+
+function updateBigCup(){
+    //count how many full cups
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    //total number of cups
+    const totalCups = smallCups.length;
+    //dont show percent if no cups are full
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden';
+        percentage.style.height = 0;  
+    }
+    //show percent when there are full cups
+    else{
+        percentage.style.visibility = 'visible';
+        percentage.style.height = `${fullCups / totalCups * 330}px`;  
+        percentage.innerText = `${fullCups / totalCups * 100}%`;
+    }
+
+    if(fullCups === totalCups){
+        remained.style.visibility = 'hidden';
+        remained.style.height = 0;  
+    }
+    else{
+        remained.style.visibility = 'visible';
+        remained.style.height = 0;  
+        liters.innerText = `${2-(250 * fullCups/1000)}L`;
+    }
+}
